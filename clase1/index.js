c3ab6d19224143257abb55797810a14578b96736
Camila Jaurena <jaurenacamila@gmail.com>
2023-04-10 21:41:36 -0300

    funciones

--- clase1/index.js
+++ clase1/index.js
@@ -1,4 +1,11 @@
 // node index.js para ejecutar en terminal
 
+//dsitintas formas de declarar funciones
 
-console.log('Camila')
+const dividir = (n1,n2) => n1/n2
+console.log(dividir(4,2))
+
+const multiplicar = (n1,n2) => {
+    return n1*n2
+}
+console.log(multiplicar(3,5))
